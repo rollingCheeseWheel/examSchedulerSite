635e97f542de10aee9b7e81141d32b016979a98d
rollingCheeseWheel <manuelsinner.ms@gmail.com>
2026-05-21 22:42:22 +0200

    tried fixing some issues because of stale references

--- src/components/common/auth/AuthCallback.tsx
+++ src/components/common/auth/AuthCallback.tsx
@@ -5,18 +5,18 @@ import { useIsLoggedIn } from "../../../hooks/useIsLoggedIn";
 import { useLoadingPromise } from "../../../hooks/useLoadingPromise";
 import { useSignalRInit } from "../../../hooks/useSignalR";
 import {
-    useClassrooms,
-    useHubConnection,
-    useLoadingOverlay,
-    useSchedules,
-    useUserProfile,
+	useClassrooms,
+	useHubConnection,
+	useLoadingOverlay,
+	useSchedules,
+	useUserProfile,
 } from "../../../hooks/zustand";
 import { api, setTokenDuration } from "../../../main";
 import { type DateString } from "../../../models/brand";
 import type { Result } from "../../../models/result";
 import type { UserProfile } from "../../../models/user";
 
-export function AuthCallback({ disabled }: { disabled?: boolean }) {
+export function AuthCallback() {
 	const setLoadingOverlay = useLoadingOverlay((s) => s.setState);
 	const [isLoggedIn, setSessionEnd] = useIsLoggedIn();
 	const navigate = useNavigate();
@@ -26,11 +26,12 @@ export function AuthCallback({ disabled }: { disabled?: boolean }) {
 			navigate({ pathname: "/auth", search: "" }, { replace: true }),
 	});
 	const initSignalR = useSignalRInit();
-	const connection = useHubConnection(s => s.data);
+	const connection = useHubConnection((s) => s.data);
 	const setSchedule = useSchedules((s) => s.set);
 	const removeSchedule = useSchedules((s) => s.removeKey);
 	const setClassroom = useClassrooms((s) => s.set);
 	const setUserProfile = useUserProfile((s) => s.setData);
+	const userprofileHasChanged = useUserProfile((s) => s.hasChanged);
 
 	const boundSignalRInit = useCallback(
 		() =>
@@ -40,8 +41,11 @@ export function AuthCallback({ disabled }: { disabled?: boolean }) {
 					setSchedule(...schedules);
 				},
 				onScheduleCreated(scheduleId) {
-					console.debug("schedule created", scheduleId)
-					resolve(connection?.subscribeSchedule(scheduleId) ?? Promise.reject(new Error("connection not initialized")));
+					console.debug("schedule created", scheduleId);
+					resolve(
+						connection?.subscribeSchedule(scheduleId) ??
+							Promise.reject(new Error("connection not initialized")),
+					);
 				},
 				onUpdateSchedule(schedule) {
 					console.debug("schedule update", schedule);
@@ -62,22 +66,25 @@ export function AuthCallback({ disabled }: { disabled?: boolean }) {
 			}),
 		[
 			initSignalR,
-			setClassroom,
 			setSchedule,
-			connection
+			resolve,
+			connection,
+			removeSchedule,
+			setClassroom,
 		],
 	);
 
 	useEffect(() => {
-		if (disabled) {
-			return;
-		}
-
 		const queryParams = new URLSearchParams(window.location.search);
 		const authCode = queryParams.get("code");
 		const schoolId = queryParams.get("school_id");
 
-		if (isLoggedIn && !authCode && !schoolId) {
+		if ((!authCode || !schoolId) && !isLoggedIn) {
+			navigate({ pathname: "/auth", search: "" }, { replace: true });
+			return;
+		}
+
+		if (!userprofileHasChanged && isLoggedIn && !authCode && !schoolId) {
 			resolve(
 				api<Result<UserProfile>>(endpoints.auth.me, {
 					method: "GET",
@@ -90,7 +97,7 @@ export function AuthCallback({ disabled }: { disabled?: boolean }) {
 					onError: () => {
 						console.error("failed to get userprofile");
 						setSessionEnd(0);
-						navigate("/auth");
+						navigate({ pathname: "/auth", search: "" }, { replace: true });
 					},
 				},
 			);
@@ -98,62 +105,74 @@ export function AuthCallback({ disabled }: { disabled?: boolean }) {
 				onError: () => {
 					console.error("error during signalr init");
 					setSessionEnd(0);
-					navigate("/auth");
+					navigate({ pathname: "/auth", search: "" }, { replace: true });
 				},
 			});
 			return;
 		}
 
-		if (!authCode || !schoolId) {
-			navigate({ pathname: "/auth", search: "" }, { replace: true });
+		if (!isLoggedIn) {
+			resolve(
+				api<Result<DateString>>(endpoints.auth.login, {
+					method: "POST",
+					body: { authCode, schoolId },
+				}),
+				{
+					onError: () =>
+						navigate({ pathname: "/auth", search: "" }, { replace: true }),
+					onSuccess: (res) => {
+						if (!res.data) {
+							console.error("error during login");
+							return;
+						}
+						setTokenDuration(new Date(res.data).getTime() - Date.now());
+						setSessionEnd(new Date(res.data).getTime());
+						resolve(
+							api<Result<UserProfile>>(endpoints.auth.me, {
+								method: "GET",
+							}),
+							{
+								onSuccess: (res) => {
+									setUserProfile(res.data);
+								},
+								onError: () => {
+									console.error("failed to get userprofile");
+									setSessionEnd(0);
+									navigate(
+										{ pathname: "/auth", search: "" },
+										{ replace: true },
+									);
+								},
+							},
+						);
+						resolve(boundSignalRInit(), {
+							onError: () => {
+								setSessionEnd(0);
+								navigate({ pathname: "/auth", search: "" }, { replace: true });
+							},
+						});
+						navigate({ pathname: "/", search: "" }, { replace: true });
+					},
+				},
+			);
 			return;
 		}
 
-		resolve(
-			api<Result<DateString>>(endpoints.auth.login, {
-				method: "POST",
-				body: { authCode, schoolId },
-			}),
-			{
-				onError: () => {
-					console.error("error during authentication");
-				},
-				onSuccess: (res) => {
-					if (!res.data) {
-						console.error("error during login");
-						return;
-					}
-					setTokenDuration(new Date(res.data).getTime() - Date.now());
-					setSessionEnd(new Date(res.data).getTime());
-					resolve(
-						api<Result<UserProfile>>(endpoints.auth.me, {
-							method: "GET",
-						}),
-						{
-							onSuccess: (res) => {
-								setUserProfile(res.data);
-							},
-							onError: () => {
-								console.error("failed to get userprofile");
-								setSessionEnd(0);
-								navigate("/auth");
-							},
-						},
-					);
-					resolve(boundSignalRInit(), {
-						onError: () => {
-							console.error("error during signalr init");
-							setSessionEnd(0);
-							navigate("/auth");
-						},
-					});
-					navigate({ pathname: "/", search: "" }, { replace: true });
-				},
+		resolve(boundSignalRInit(), {
+			onError: () => {
+				setSessionEnd(0);
+				navigate({ pathname: "/auth", search: "" }, { replace: true });
 			},
-		);
-
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+		});
+	}, [
+		boundSignalRInit,
+		isLoggedIn,
+		navigate,
+		resolve,
+		setSessionEnd,
+		setUserProfile,
+		userprofileHasChanged,
+	]);
 
 	return <></>;
 }
